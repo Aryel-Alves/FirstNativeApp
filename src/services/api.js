413e95cecf787be8101a74758febe0c7ea6e76bb
Aryel Alves <Aryel-Alves@users.noreply.github.com>
2019-07-16 23:57:14 -0300

    final

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://appninja.com/api'
+    baseURL: 'http://rocketseat-node.herokuapp.com/api' // http://172.17.91.209:8080/api
 })
 
 export default api
